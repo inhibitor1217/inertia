10d8bb9298f69d33942a84439267306094413141
dugi <dugi@channel.io>
2025-02-08 20:01:16 +0900

    feat: favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,10 +16,16 @@ export default function RootLayout({
   return (
     <html>
       <head>
-      <link
-        rel="stylesheet"
-        href="https://cf.channel.io/asset/font/Inter/inter.css"
-      />
+        <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <meta name="apple-mobile-web-app-title" content="Inertia" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link
+          rel="stylesheet"
+          href="https://cf.channel.io/asset/font/Inter/inter.css"
+        />
       </head>
       <body>
         <Provider>
