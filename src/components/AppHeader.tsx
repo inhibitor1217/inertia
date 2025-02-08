a23045606cc4329c2fae170a518fc1ed04046920
dugi <dugi@channel.io>
2025-02-08 20:44:48 +0900

    feat: add logo

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -1,4 +1,5 @@
 import { Text } from "@channel.io/bezier-react"
+import Image from "next/image"
 import Link from "next/link"
 
 import styles from "./AppHeader.module.css"
@@ -7,6 +8,13 @@ export default function AppHeader() {
   return (
     <div className={styles.container}>
       <Link href="/" className={styles.brand}>
+        <Image
+          src="/favicon.svg"
+          alt="Inertia logo"
+          width={32}
+          height={32}
+          className={styles.logo}
+        />
         <Text
           as="h1"
           typo="22"
