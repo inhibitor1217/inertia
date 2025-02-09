87baf18c2a60ddb5747532cb3e11d07ba8ed9c69
dugi <dugi@channel.io>
2025-02-09 14:37:54 +0900

    feat: pwa manifest

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,6 @@ export default function RootLayout({
         <link rel="shortcut icon" href="/favicon.ico" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <meta name="apple-mobile-web-app-title" content="Inertia" />
-        <link rel="manifest" href="/site.webmanifest" />
         <link
           rel="stylesheet"
           href="https://cf.channel.io/asset/font/Inter/inter.css"
