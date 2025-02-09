f341c4f3da3b9b6c49327cabdad9afc24f3ea5f7
dugi <dugi@channel.io>
2025-02-09 22:17:09 +0900

    feat: add pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,5 @@
 export default function Home() {
   return (
-    <>
-      <h1>Inertia</h1>
-      {Array.from({ length: 20 }).map((_, i) => (
-        <div key={i} style={{ padding: "20px" }}>
-          <h2>Section {i + 1}</h2>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua.
-          </p>
-        </div>
-      ))}
-    </>
+    <></>
   )
 }
